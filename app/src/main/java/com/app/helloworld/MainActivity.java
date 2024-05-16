d229fd7a09668166c62cb2d920964aa8d98113ee
2324 <2324139312@qq.com>
2024-05-16 17:41:22 +0800

    First commit

---
+++ app/src/main/java/com/app/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.app.helloworld;
+
+import android.os.Bundle;
+import android.util.Log;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "onCreate: excute");
+
+    }
+}
